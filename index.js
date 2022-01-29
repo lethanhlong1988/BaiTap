83fe300139ca559a8b13918fc48a8d2fa95257fd
lethanhlong1988 <lethanhlong2008@gmail.com>
2022-01-29 16:26:19 +0900

    add file

---
+++ index.js
@@ -0,0 +1,29 @@
+
+// var fullName = 'React Native 04'
+
+// console.log(fullName);
+
+// function sum(a, b){
+// // var a = 5;
+// // var b = 6;
+// return a + b;
+
+// }
+
+// var result = sum(7, 2);
+//  console.log(result);
+
+// function logUsename(){
+// var username = 'React Native';
+// var lop = 4;
+
+// console.log(username +' 0'+ lop);
+// return username;
+
+// }
+
+// usename = logUsename();
+// console.log(username);
+// console.log(lop);
+
+  
